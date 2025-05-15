2ed31699311fab72ae672627cc6dab2412f4696f
atlascharm <a22980491@gmail.com>
2025-05-15 18:44:43 +0100

    Add files via upload

---
+++ atlas charm web site/dinamique.js
@@ -0,0 +1,7 @@
+function redirectToWhatsApp() {
+    window.location.href = "https://wa.me/0638456228";
+}
+function redirectToWhatsApp() {
+    window.location.href = "https://wa.me/your-phone-number?text=I%20want%20to%20buy%20the%20ATLAS%20CHARM%20car%20air%20freshener!";
+}
+
